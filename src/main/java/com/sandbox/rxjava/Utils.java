e0349505266516dbea8248513f3f3c1b838305c3
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2020-08-21 18:38:19 -0300

    Add util to wrap support utilities

---
+++ src/main/java/com/sandbox/rxjava/Utils.java
@@ -0,0 +1,21 @@
+package com.sandbox.rxjava;
+
+import java.util.concurrent.TimeUnit;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public abstract class Utils
+{
+    public static void sleepOneSecond()
+    {
+        try
+        {
+            TimeUnit.SECONDS.sleep(1);
+        }
+        catch (InterruptedException e)
+        {
+            log.error("Exception to sleep ", e);
+        }
+    }
+}
