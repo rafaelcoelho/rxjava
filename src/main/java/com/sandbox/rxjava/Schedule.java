378e1d115b7291fc57c584415c71942f5c7eddad
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2020-08-21 18:39:20 -0300

    Add schedule to try trampoline behavior

---
+++ src/main/java/com/sandbox/rxjava/Schedule.java
@@ -0,0 +1,36 @@
+package com.sandbox.rxjava;
+
+import static com.sandbox.rxjava.Utils.sleepOneSecond;
+
+import io.reactivex.rxjava3.core.Scheduler;
+import io.reactivex.rxjava3.core.Scheduler.Worker;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public final class Schedule
+{
+    public static void main(String[] args)
+    {
+        final Scheduler schedule = Schedulers.trampoline();
+        final Worker worker = schedule.createWorker();
+
+        log.debug("Main start");
+
+        worker.schedule(() -> {
+            log.debug(" Outer start");
+            sleepOneSecond();
+
+            worker.schedule(() -> {
+                log.debug("  Inner start");
+                sleepOneSecond();
+                log.debug("  Inner end");
+            });
+            log.debug(" Outer end");
+        });
+
+        log.debug("Main end");
+
+        worker.dispose();
+    }
+}
